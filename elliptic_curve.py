bc09276bf0f844afb6e4ada3e1614a305ba9fc3c
Petar Tesic <petar.tesic@syrmia.com>
2023-07-03 16:31:31 +0200

    Half of template added for the point on elliptic curve!

---
+++ elliptic_curve.py
@@ -0,0 +1,13 @@
+class Point:
+	def __init__(self):
+		print('Point on a elliptic curve')
+	
+	def __str__(self):
+		return f'Elliptic curve point!'
+
+def main():
+
+
+
+if __name__=="main":
+	main()					
